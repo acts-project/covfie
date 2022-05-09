3805ac90703872e9904ff16e6f34caa06b00a331
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2022-05-09 16:21:56 +0200

    Add C++17 compatible support for constraints
    C++ constraints based on concepts are a C++20 feature, but they would be
    tremendously powerful for covfie. Therefore, this commit adds a sort of
    middleground where we can optionally use concepts without breaking C++17
    support. This works by defining a new macro `CONSTRAINT(concept)`, which
    evaluates to a concept constraint on C++20 compilers, and evaluates
    simply to the kind `typename` on older (C++17) compilers.

---
+++ lib/core/covfie/core/concepts.hpp
@@ -0,0 +1,20 @@
+/*
+ * This file is part of covfie, a part of the ACTS project
+ *
+ * Copyright (c) 2022 CERN
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <covfie/core/definitions.hpp>
+
+#if __cpp_concepts >= 201907L
+namespace covfie::concepts {
+template <typename T>
+concept field_backend = true;
+}
+#endif
