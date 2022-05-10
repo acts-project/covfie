1273ada6ea6872ee1c9b3ecf2ad6c6915f074a1e
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2022-05-10 10:55:19 +0200

    Add a useful conversion from tuple to array
    This is useful in combination with the n-dimensonal map, which produces
    tuples. Being able to convert those to arrays or coordinates we can use
    elsewhere is important.

---
+++ lib/core/covfie/core/utility/tuple.hpp
@@ -0,0 +1,27 @@
+/*
+ * This file is part of covfie, a part of the ACTS project
+ *
+ * Copyright (c) 2022 CERN
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <array>
+#include <functional>
+
+namespace covfie::utility {
+template <typename T, typename... Ts>
+std::array<T, sizeof...(Ts) + 1u> to_array(std::tuple<T, Ts...> t)
+{
+    return std::apply(
+        [](T t, Ts... ts) {
+            return std::array<T, sizeof...(Ts) + 1u>{t, ts...};
+        },
+        t
+    );
+}
+}
