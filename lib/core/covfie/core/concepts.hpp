ad3ddd60f20b418b9803e4e44859f32856cf2361
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2022-05-19 09:55:46 +0200

    Expand on the concept for field backends
    The field backend concept previously checked only for the presence of
    certain typedefs, and not for important member functions. This commit
    adds additional requirements to the concept to make sure that the
    necessary member functions are present.

--- lib/core/covfie/core/concepts.hpp
+++ lib/core/covfie/core/concepts.hpp
@@ -10,17 +10,67 @@
 
 #pragma once
 
+#include <fstream>
+
 #include <covfie/core/definitions.hpp>
 
 #if __cpp_concepts >= 201907L
+#include <concepts>
 namespace covfie::concepts {
 template <typename T>
 concept field_backend = requires
 {
+    /*
+     * Check whether the backend has the required type definitions, which are
+     * the input and output types of the contravariant and covariant parts of
+     * the layer.
+     */
     typename T::contravariant_input_t;
     typename T::contravariant_output_t;
     typename T::covariant_input_t;
     typename T::covariant_output_t;
+
+    /*
+     * Confirm that the backend has both an owning and a non-owning data type.
+     */
+    typename T::owning_data_t;
+    typename T::non_owning_data_t;
+
+    /*
+     * Check whether the owning data type can be read from a file.
+     */
+    requires requires(std::ifstream & fs)
+    {
+        {typename T::owning_data_t(fs)};
+    };
+
+    requires requires(const typename T::owning_data_t & d)
+    {
+        /*
+         * Check whether a non-owning data type can be constructed from the
+         * owning variant.
+         */
+        {typename T::non_owning_data_t(d)};
+
+        /*
+         * Check whether an owning data type can be written to disk.
+         */
+        requires requires(std::ofstream & fs)
+        {
+            {d.dump(fs)};
+        };
+    };
+
+    /*
+     * Check whether a non-owning object allows us to look up the magnetic
+     * field, and whether that operation gives the correct result.
+     */
+    requires requires(const typename T::non_owning_data_t & d)
+    {
+        {
+            d.at(std::declval<typename T::contravariant_input_t::vector_t>())
+            } -> std::same_as<typename T::covariant_output_t::vector_t>;
+    };
 };
 
 template <typename T>
