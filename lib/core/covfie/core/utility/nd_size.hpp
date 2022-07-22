791c5fcfea44d7fead279aa8c8de7bef969a7c31
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2022-07-22 18:28:10 +0200

    Add general n-dimensional size type
    Previously this was sort of all over the place, now it is a consistent
    type which we can re-use everywhere. Even if it is just a simple
    typedef!

---
+++ lib/core/covfie/core/utility/nd_size.hpp
@@ -0,0 +1,18 @@
+/*
+ * This file is part of covfie, a part of the ACTS project
+ *
+ * Copyright (c) 2022 CERN
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <array>
+
+namespace covfie::utility {
+template <std::size_t N>
+using nd_size = std::array<std::size_t, N>;
+}
