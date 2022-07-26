61b1f84b250ac1db725a91a10ea01fc742000900
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2022-07-26 13:27:50 +0200

    Add support for ATLAS magnetic field in benchmarks
    This commit adds helper functions and definitions that will allow
    benchmarks to access and use the ATLAS magnetic field.

---
+++ benchmarks/common/atlas.cpp
@@ -0,0 +1,34 @@
+/*
+ * This file is part of covfie, a part of the ACTS project
+ *
+ * Copyright (c) 2022 CERN
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <cstdlib>
+
+#include <covfie/benchmark/atlas.hpp>
+
+std::unique_ptr<atlas_field_t> ATLAS_FIELD;
+
+const atlas_field_t & get_atlas_field()
+{
+    if (!ATLAS_FIELD) {
+        char * file_name = std::getenv("COVFIE_BENCHMARK_ATLAS_FIELD");
+
+        if (file_name) {
+            std::ifstream ifs(file_name, std::ifstream::binary);
+            ATLAS_FIELD = std::make_unique<atlas_field_t>(ifs);
+        } else {
+            throw std::runtime_error(
+                "Environment variable \"COVFIE_BENCHMARK_ATLAS_FIELD\" is not "
+                "set!"
+            );
+        }
+    }
+
+    return *ATLAS_FIELD.get();
+}
---
+++ benchmarks/common/covfie/benchmark/atlas.hpp
@@ -0,0 +1,27 @@
+/*
+ * This file is part of covfie, a part of the ACTS project
+ *
+ * Copyright (c) 2022 CERN
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <memory>
+
+#include <covfie/core/backend/initial/array.hpp>
+#include <covfie/core/backend/transformer/affine.hpp>
+#include <covfie/core/backend/transformer/interpolator/nearest_neighbour.hpp>
+#include <covfie/core/backend/transformer/layout/strided.hpp>
+#include <covfie/core/field.hpp>
+
+using atlas_field_t = covfie::field<covfie::backend::transformer::affine<
+    covfie::backend::transformer::interpolator::nearest_neighbour<
+        covfie::backend::layout::strided<
+            covfie::vector::ulong3,
+            covfie::backend::storage::array<covfie::vector::float3>>>>>;
+
+extern std::unique_ptr<atlas_field_t> ATLAS_FIELD;
+
+const atlas_field_t & get_atlas_field();
