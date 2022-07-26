9575c666d79beff5a5afff6581157285ea886819
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2022-07-26 13:31:27 +0200

    Add a new depth-first Lorentz-Euler access pattern
    Previously, we only had a breadth first access pattern, but it is
    interesting to analyse the performance of backends for depth-first
    patterns as well.

--- benchmarks/cpu/patterns/lorentz_euler.hpp
+++ benchmarks/cpu/patterns/lorentz_euler.hpp
@@ -98,3 +98,89 @@ struct LorentzEulerWide : covfie::benchmark::AccessPattern<LorentzEulerWide> {
             p.particles * p.steps * 21};
     }
 };
+
+struct LorentzEulerDeep : covfie::benchmark::AccessPattern<LorentzEulerDeep> {
+    struct parameters {
+        std::size_t particles, steps;
+    };
+
+    static constexpr std::array<std::string_view, 2> parameter_names = {
+        "N", "S"};
+    static constexpr std::string_view name = "LorentzEulerDeep";
+
+    template <typename backend_t>
+    static void iteration(
+        const parameters & p,
+        const covfie::field_view<backend_t> & f,
+        ::benchmark::State & state
+    )
+    {
+        state.PauseTiming();
+
+        std::random_device rd;
+        std::mt19937 e(rd());
+
+        std::uniform_real_distribution<> phi_dist(0.f, 2.f * 3.1415927f);
+        std::uniform_real_distribution<> costheta_dist(-1.f, 1.f);
+
+        float ss = 0.001f;
+
+        std::vector<covfie::benchmark::lorentz_agent<3>> objs(p.particles);
+
+        for (std::size_t i = 0; i < p.particles; ++i) {
+            float theta = std::acos(costheta_dist(e));
+            float phi = phi_dist(e);
+
+            objs[i].pos[0] = 0.f;
+            objs[i].pos[1] = 0.f;
+            objs[i].pos[2] = 0.f;
+
+            objs[i].mom[0] = 100 * std::sin(theta) * std::cos(phi);
+            objs[i].mom[1] = 100 * std::sin(theta) * std::sin(phi);
+            objs[i].mom[2] = 100 * std::cos(theta);
+        }
+
+        state.ResumeTiming();
+
+        for (std::size_t i = 0; i < p.particles; ++i) {
+            for (std::size_t s = 0; s < p.steps; ++s) {
+                covfie::benchmark::lorentz_agent<3> & o = objs[i];
+                typename std::decay_t<decltype(f)>::output_t b =
+                    f.at(o.pos[0], o.pos[1], o.pos[2]);
+                float f[3] = {
+                    o.mom[1] * b[2] - o.mom[2] * b[1],
+                    o.mom[2] * b[0] - o.mom[0] * b[2],
+                    o.mom[0] * b[1] - o.mom[1] * b[0]};
+
+                o.pos[0] += o.mom[0] * ss;
+                o.pos[1] += o.mom[1] * ss;
+                o.pos[2] += o.mom[2] * ss;
+
+                o.mom[0] += f[0] * ss;
+                o.mom[1] += f[1] * ss;
+                o.mom[2] += f[2] * ss;
+            }
+        }
+    }
+
+    static std::vector<std::vector<int64_t>> get_parameter_ranges()
+    {
+        return {{1, 8, 64, 512, 4096}, {1, 8, 64, 512, 4096}};
+    }
+
+    static parameters get_parameters(benchmark::State & state)
+    {
+        return {
+            static_cast<std::size_t>(state.range(0)),
+            static_cast<std::size_t>(state.range(1))};
+    }
+
+    template <typename S, std::size_t N>
+    static covfie::benchmark::counters get_counters(const parameters & p)
+    {
+        return {
+            p.particles * p.steps,
+            p.particles * p.steps * N * sizeof(S),
+            p.particles * p.steps * 21};
+    }
+};
