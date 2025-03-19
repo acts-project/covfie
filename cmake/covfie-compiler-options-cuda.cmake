1741babd3c93c7b158b66a0759a391c04c115ebe
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2025-03-19 14:09:57 +0100

    Prevent CMake flags from propagating to parent
    See https://github.com/acts-project/detray/pull/956.

--- cmake/covfie-compiler-options-cuda.cmake
+++ cmake/covfie-compiler-options-cuda.cmake
@@ -3,52 +3,54 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-# FindCUDAToolkit needs at least CMake 3.17.
-cmake_minimum_required(VERSION 3.17)
+cmake_minimum_required(VERSION 3.21)
 
-# Include the helper function(s).
-include(covfie-functions)
+# Only set these compiler flags if we are the top level project.
+if(PROJECT_IS_TOP_LEVEL)
+    # Include the helper function(s).
+    include(covfie-functions)
 
-# Figure out the properties of CUDA being used.
-find_package(CUDAToolkit REQUIRED)
+    # Figure out the properties of CUDA being used.
+    find_package(CUDAToolkit REQUIRED)
 
-# Set the architecture to build code for.
-set(CMAKE_CUDA_ARCHITECTURES
-    "52"
-    CACHE STRING
-    "CUDA architectures to build device code for"
-)
-
-# Turn on the correct setting for the __cplusplus macro with MSVC.
-if("${CMAKE_CXX_COMPILER_ID}" MATCHES "MSVC")
-    covfie_add_flag( CMAKE_CUDA_FLAGS "-Xcompiler /Zc:__cplusplus" )
-endif()
-
-if("${CMAKE_CUDA_COMPILER_ID}" MATCHES "NVIDIA")
-    # Make CUDA generate debug symbols for the device code as well in a debug
-    # build.
-    covfie_add_flag( CMAKE_CUDA_FLAGS_DEBUG "-G" )
-endif()
-
-covfie_add_flag( CMAKE_CUDA_FLAGS "-Wfloat-conversion" )
-covfie_add_flag( CMAKE_CUDA_FLAGS "-Wconversion" )
-
-# Fail on warnings, if asked for that behaviour.
-if(COVFIE_FAIL_ON_WARNINGS)
-    if(
-        (
-            "${CUDAToolkit_VERSION}"
-                VERSION_GREATER_EQUAL
-                "10.2"
-        )
-        AND (
-            "${CMAKE_CUDA_COMPILER_ID}"
-                MATCHES
-                "NVIDIA"
-        )
+    # Set the architecture to build code for.
+    set(CMAKE_CUDA_ARCHITECTURES
+        "52"
+        CACHE STRING
+        "CUDA architectures to build device code for"
     )
-        covfie_add_flag( CMAKE_CUDA_FLAGS "-Werror all-warnings" )
-    elseif("${CMAKE_CUDA_COMPILER_ID}" MATCHES "Clang")
-        covfie_add_flag( CMAKE_CUDA_FLAGS "-Werror" )
+
+    # Turn on the correct setting for the __cplusplus macro with MSVC.
+    if("${CMAKE_CXX_COMPILER_ID}" MATCHES "MSVC")
+        covfie_add_flag( CMAKE_CUDA_FLAGS "-Xcompiler /Zc:__cplusplus" )
+    endif()
+
+    if("${CMAKE_CUDA_COMPILER_ID}" MATCHES "NVIDIA")
+        # Make CUDA generate debug symbols for the device code as well in a debug
+        # build.
+        covfie_add_flag( CMAKE_CUDA_FLAGS_DEBUG "-G" )
+    endif()
+
+    covfie_add_flag( CMAKE_CUDA_FLAGS "-Wfloat-conversion" )
+    covfie_add_flag( CMAKE_CUDA_FLAGS "-Wconversion" )
+
+    # Fail on warnings, if asked for that behaviour.
+    if(COVFIE_FAIL_ON_WARNINGS)
+        if(
+            (
+                "${CUDAToolkit_VERSION}"
+                    VERSION_GREATER_EQUAL
+                    "10.2"
+            )
+            AND (
+                "${CMAKE_CUDA_COMPILER_ID}"
+                    MATCHES
+                    "NVIDIA"
+            )
+        )
+            covfie_add_flag( CMAKE_CUDA_FLAGS "-Werror all-warnings" )
+        elseif("${CMAKE_CUDA_COMPILER_ID}" MATCHES "Clang")
+            covfie_add_flag( CMAKE_CUDA_FLAGS "-Werror" )
+        endif()
     endif()
 endif()
